3f1453e922eeaeceed81d9bad7046d2bdd1e5ac2
FtyLollipop <ftylollipop@outlook.com>
2023-04-20 19:46:38 +0800

    Update PlayerStatsTracker.js

--- PlayerStatsTracker.js
+++ PlayerStatsTracker.js
@@ -7,7 +7,7 @@ const defaultConfig = {
   totem: true,
   destroy: true,
   place: true,
-  sprinting : true,
+  sprinting: true,
   xp: true
 }
 const config = data.parseJson(new JsonConfigFile('plugins/PlayerStatsTracker/config.json', data.toJson(defaultConfig)))
@@ -31,6 +31,11 @@ mc.listen('onPlayerDie', (player, source) => {
 
 })
 
+// 生物死亡
+mc.listen('onMobDie', (mob, source, cause) => {
+
+})
+
 // 聊天
 mc.listen('onChat', (player, msg) => {
 
@@ -43,6 +48,21 @@ mc.listen('onPlayerJump', (player) => {
 
 // 潜行
 mc.listen('onSneak', (player, isSneaking) => {
+
+})
+
+// 使用物品
+mc.listen('onUseItem', (player, item) => {
+
+})
+
+// 生物受伤
+mc.listen('onMobHurt', (mob, source, damage, cause) => {
+
+})
+
+// 骑乘 尝试骑乘的实体 被骑乘的实体
+mc.listen('onRide', (entity1, entity2) => {
 
 })
 
@@ -62,7 +82,7 @@ mc.listen('onDestroyBlock', (player) => {
 })
 
 // 放置方块
-mc.listen('onPlaceBlock', (player) => {
+mc.listen('afterPlaceBlock', (player) => {
 
 })
 
@@ -73,5 +93,20 @@ mc.listen('onChangeSprinting', (player, sprinting) => {
 
 // 获得经验
 mc.listen('onExperienceAdd', (player, sprinting) => {
+
+})
+
+// 弹射物创建完毕
+mc.listen('onProjectileCreated', (shooter, entity) => {
+
+})
+
+// 方块被弹射物击中
+mc.listen('onProjectileHitBlock', (block, source) => {
+
+})
+
+// 实体被弹射物击中
+mc.listen('onProjectileHitEntity', (entity, source) => {
 
 })
