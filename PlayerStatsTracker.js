a1a0a179f55dad4e965a45f586f50794c454a0e5
FtyLollipop <ftylollipop@outlook.com>
2023-04-21 15:33:10 +0800

    Update PlayerStatsTracker.js

--- PlayerStatsTracker.js
+++ PlayerStatsTracker.js
@@ -1,53 +1,172 @@
 const defaultConfig = {
-  death: true,
-  chat: true,
-  jump: true,
-  sneak: true,
-  eat: true,
-  totem: true,
-  destroy: true,
-  place: true,
-  sprinting: true,
-  xp: true
 }
-const defaultPlayerData = {
-  death: 0,
-  kill: 0,
-  killedMob: {
-    "house": 0
-  },
-  destroy: 0,
-  place: 0,
-  eat: 0,
-  totem: 0,
-  xp: 0,
-  level: 0,
 
-}
 const config = data.parseJson(new JsonConfigFile('plugins/PlayerStatsTracker/config.json', data.toJson(defaultConfig)))
+const crops = [
+  'mineraft:weat',
+  'mineraft:potatoes',
+  'mineraft:carrots',
+  'minecraft:melon_stem',
+  'minecraft:pumpkin_stem',
+  'minecraft:beetroot',
+  'minecraft:pitcher_crop',
+  'minecraft:torchflower_crop'
+]
 
 ll.registerPlugin('PlayerStatsTracker', 'Track player stats.', [1, 0, 0])
 
-let db = new KVDatabase('./plugins/PlayerStatsTracker/data')
-if (!db) {
-  logger.error('数据库文件打开失败')
-}
+let db = new DataBase('./plugins/PlayerStatsTracker/data')
 
-function getDB(name) {
-  let data = db.get(name)
-  if (data) {
-    return data
-  } else {
-    if (!db.set(name, defaultPlayerData)) {
-      logger.error('数据库写入失败')
-      return null
+class DataBase {
+  defaultPlayerData
+  kvdb
+
+  constructor(str) {
+    this.defaultPlayerData = {
+      death: 0,
+      killed: 0,
+      damageTaken: 0,
+      damageDealt: 0,
+      destroyed: 0,
+      placed: 0,
+      ate: 0,
+      totem: 0,
+      expObtained: 0,
+      highestLevel: 0,
+      playTime: 0,
+      distanceWalked: 0,
+      distanceFlown: 0,
+      subStats: {
+        ate: {
+          "minecraft:golden_apple": 0,
+          "minecraft:enchanted_golden_apple": 0
+        },
+        killed: {
+          "minecraft:horse": 0
+        },
+        planted: {
+          'mineraft:weat': 0,
+          'mineraft:potatoes': 0,
+          'mineraft:carrots': 0,
+          'minecraft:melon_stem': 0,
+          'minecraft:pumpkin_stem': 0,
+          'minecraft:beetroot': 0,
+          'minecraft:pitcher_crop': 0,
+          'minecraft:torchflower_crop': 0
+        }
+      }
     }
-    return this(name)
+    this.kvdb = new KVDatabase(str)
+    if (!this.kvdb) {
+      this.readErr()
+    }
   }
-}
 
-function setDB(name,value) {
-  
+  readErr() {
+    logger.error('打开数据库文件失败')
+  }
+
+  writeErr() {
+    logger.error('写入数据库文件失败')
+  }
+
+  // 洗数据
+  cleanData(playerData) {
+    let cleanedData = Object.assign(defaultPlayerData, playerData)
+    cleanedData.subStats = Object.assign(defaultPlayerData.subStats, playerData.subStats)
+    Object.keys(cleanedData.subStats).forEach(key => {
+      cleanedData.subStats[key] = Object.assign(defaultPlayerData.subStats[key], playerData.subStats[key])
+    });
+    return cleanedData
+  }
+
+  // 获取玩家某一项统计
+  get(name, key) {
+    return Object.assign(defaultPlayerData, this.kvdb.get(name))?.[key]
+  }
+
+  // 获取玩家所有统计
+  getPlayer(name) {
+    return this.cleanData(this.kvdb.get(name))
+  }
+
+  // 设置玩家某一项统计
+  set(name, key, operator, value) {
+    const playerData = this.kvdb.get(name)
+    if (!playerData?.[key]) {
+      if (!this.kvdb.set(name, Object.assign(defaultPlayerData, playerData))) {
+        this.writeErr()
+        return false
+      }
+      return this.set(name, key, operator, value)
+    }
+    switch (operator) {
+      case 'set':
+        playerData[key] = value
+        break
+      case 'add':
+        playerData[key] += value
+        break
+      case 'reduce':
+        playerData[key] -= value
+        break
+      default:
+        return false
+    }
+    if (!this.kvdb.set(name, Object.assign(defaultPlayerData, playerData))) {
+      this.writeErr()
+      return false
+    }
+    return true
+  }
+
+  // 设置某一子项统计
+  setSub(name, key, subKey, operator, value) {
+    const playerData = this.getPlayer(name)
+    if (!playerData?.[key]?.[subKey]) {
+      if (!this.kvdb.set(name, this.cleanData(playerData))) {
+        this.writeErr()
+        return false
+      }
+      return this.set(name, key, subKey, operator, value)
+    }
+    switch (operator) {
+      case 'set':
+        playerData[key][subKey] = value
+        break
+      case 'add':
+        playerData[key][subKey] += value
+        break
+      case 'reduce':
+        playerData[key][subKey] -= value
+        break
+      default:
+        return false
+    }
+    if (!this.kvdb.set(name, Object.assign(defaultPlayerData, playerData))) {
+      this.writeErr()
+      return false
+    }
+    return true
+  }
+
+  // 设置玩家所有统计
+  setPlayer(name, playerData) {
+    if (!this.kvdb.set(name, this.cleanData(playerData))) {
+      this.writeErr()
+      return false
+    }
+    return true
+  }
+
+  // 删除某个玩家统计信息
+  removePlayer(name) {
+    if (!this.kvdb.remove(name)) {
+      this.writeErr()
+      return false
+    }
+    return true
+  }
 }
 
 // 进入游戏
@@ -62,22 +181,33 @@ mc.listen('onLeft', (player) => {
 
 // 死亡
 mc.listen('onPlayerDie', (player, source) => {
-
+  if (!player.isSimulatedPlayer()) {
+    db.set(player.realName, 'death', 'add', 1)
+  }
 })
 
 // 生物死亡
 mc.listen('onMobDie', (mob, source, cause) => {
-
+  if (source?.isPlayer()) {
+    const pl = source.toPlayer()
+    if (!pl.isSimulatedPlayer()) {
+      db.set(pl.realName, 'kill', 'add', 1)
+    }
+  }
 })
 
 // 聊天
 mc.listen('onChat', (player, msg) => {
-
+  if (!player.isSimulatedPlayer()) {
+    db.set(player.realName, 'chat', 'add', 1)
+  }
 })
 
 // 跳跃
 mc.listen('onPlayerJump', (player) => {
-
+  if (!player.isSimulatedPlayer()) {
+    db.set(player.realName, 'jump', 'add', 1)
+  }
 })
 
 // 潜行
@@ -92,7 +222,18 @@ mc.listen('onUseItem', (player, item) => {
 
 // 生物受伤
 mc.listen('onMobHurt', (mob, source, damage, cause) => {
-
+  if (mob?.isPlayer()) {
+    const pl = mob.toPlayer()
+    if (!pl.isSimulatedPlayer()) {
+      db.set(pl.realName, 'damageTaken', 'add', damage)
+    }
+  }
+  if (source?.isPlayer()) {
+    const pl = source.toPlayer()
+    if (!pl.isSimulatedPlayer()) {
+      db.set(pl.realName, 'damageDealt', 'add', damage)
+    }
+  }
 })
 
 // 骑乘 尝试骑乘的实体 被骑乘的实体
@@ -102,22 +243,33 @@ mc.listen('onRide', (entity1, entity2) => {
 
 // 吃食物
 mc.listen('onAte', (player, item) => {
-
+  if (!player.isSimulatedPlayer()) {
+    db.set(player.realName, 'ate', 'add', 1)
+  }
 })
 
 // 消耗图腾
 mc.listen('onConsumeTotem', (player) => {
-
+  if (!player.isSimulatedPlayer()) {
+    db.set(player.realName, 'totem', 'add', 1)
+  }
 })
 
 // 破坏方块
-mc.listen('onDestroyBlock', (player) => {
-
+mc.listen('onDestroyBlock', (player, block) => {
+  if (!player.isSimulatedPlayer()) {
+    db.set(player.realName, 'destroyed', 'add', 1)
+  }
 })
 
 // 放置方块
-mc.listen('afterPlaceBlock', (player) => {
-
+mc.listen('afterPlaceBlock', (player, block) => {
+  if (!player.isSimulatedPlayer()) {
+    db.set(player.realName, 'placed', 'add', 1)
+    if (crops.includes(block.type)) {
+      db.set(player.realName, 'planted', 'add', 1)
+    }
+  }
 })
 
 // 疾跑
@@ -126,8 +278,14 @@ mc.listen('onChangeSprinting', (player, sprinting) => {
 })
 
 // 获得经验
-mc.listen('onExperienceAdd', (player, sprinting) => {
-
+mc.listen('onExperienceAdd', (player, exp) => {
+  if (!player.isSimulatedPlayer()) {
+    db.set(player.realName, 'expObtained', 'add', exp)
+    const playerLevel = player.getLevel()
+    if (playerLevel > db.get(player.realName, 'highestLevel')) {
+      db.set(player.realName, 'highestLevel', 'set', playerLevel)
+    }
+  }
 })
 
 // 弹射物创建完毕
